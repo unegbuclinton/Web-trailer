02c59170f0a2d782f744e18588b50f8daaadbaff
Unegbu Clinton <unegbuclinton5@gmail.com>
2021-10-04 07:13:28 +0000

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
     return (
         <div className ='hero--container'>
             <video src="/videos/video-2.mp4" autoPlay loop muted/>
-            <h1>ADVENTURE AWAITS</h1>
+            <h1> CLINTON ADVENTURE </h1>
             <p>What are you waiting for?
             <div className='hero-btns'>
                 
